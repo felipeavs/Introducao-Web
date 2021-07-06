6be2ae15b0b27272aa8a1a8020b151b7be26f514
Felipe Silva <felipeavs95@gmail.com>
2021-07-05 23:14:34 -0300

    trabalho concluido

--- JavaScript/Aula05/script.js
+++ JavaScript/Aula05/script.js
@@ -1,26 +1,21 @@
-let status = false;
-
 function onClick(){
     
     const imagem = document.querySelector('img');
     const button = document.querySelector('button');
     const body = document.querySelector('body');
 
-    if(!status){
-        imagem.src = 'imagens/Ligada.png';
-        status = true;
-        body.style.backgroundColor = 'white';
-        button.style.backgroundColor = 'tomato';
-        button.firstChild.data = 'Desligar';
+    if(button.className === 'button-off'){
+        button.classList.remove('button-off');
+        body.classList.remove('body-off');
+        button.firstChild.data = 'Ligar';
+        imagem.src = 'imagens/Desligada.png';
     }
     else{
-        imagem.src = 'imagens/Desligada.png';
-        status = false;
-        body.style.backgroundColor = 'silver'
-        button.style.backgroundColor = 'lightgreen';
-        button.firstChild.data = 'Ligar';
-    }
-    
+        button.classList.add('button-off');
+        body.classList.add('body-off');
+        button.firstChild.data = 'Desligar';
+        imagem.src = 'imagens/Ligada.png';
+    }    
 }
 
 const button = document.querySelector('button');
